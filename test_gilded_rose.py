ac5b40e6808029c26a7dc5d2518b9bcd5057e95d
Guotong Liao <815190191@qq.com>
2024-10-01 21:57:43 -0700

    feat: failed test

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,12 +5,24 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+
+    def test_conjured_quality_degrades_twice_as_fast(self):
+        items = [Item("Conjured", 15, 6)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual(items[0].quality, 4)
 
+    def test_conjured_quality_degrades_twice_as_fast_after_sell_date(self):
+        items = [Item("Conjured", 0, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 2)
+
+    def test_conjured_quality_never_negative(self):
+        items = [Item("Conjured", 0, -2)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertGreaterEqual(items[0].quality, 0)
 
 if __name__ == '__main__':
     unittest.main()
